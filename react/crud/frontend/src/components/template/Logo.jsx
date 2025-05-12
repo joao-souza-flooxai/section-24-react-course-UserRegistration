48e07a9038a7cc2c644abdcc5ba582a33a2754ec
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-05-12 15:17:36 -0300

    feat: Added userCrud, Finished the Section.

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -1,11 +1,12 @@
 import './Logo.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../../assets/img/logo.webp"
-
 export default (props) => (
     <aside className="logo">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img src={logo} alt="logo" />
-        </a>
+            Home
+        </Link>
     </aside>
 );
